a7fd59c5a904cf138d7f34e73c256345ca520ce9
Javier Rivera <javier.rivera.electronics@gmail.com>
2023-11-29 00:00:03 -0500

    Agregue una aclaracion para el grupo

--- app/src/main/java/com/example/ecogardenapp2/clasesLogReg/SpinCiudades.java
+++ app/src/main/java/com/example/ecogardenapp2/clasesLogReg/SpinCiudades.java
@@ -9,6 +9,12 @@ public class SpinCiudades {
    public static String[] ciudades = {"Bogotá", "Medellín", "Cali", "Barranquilla", "Cartagena",
             "Bucaramanga", "Pereira", "Ibagué", "Manizales", "Villavicencio"};
 
+   /*
+    * chicas para poder enviar los datos desde un array o una lista,
+    * a un spinner debemos usar un adaptador, se debe obtener el contexto,
+    * indicar el tipo de adaptador, e indicar la forma en que se muestra.
+    * En este caso el método es de tipo "ArrayAdapter", y retorna el objeto "adapter".
+    */
    public ArrayAdapter<String> adaptadorCiudades(Context activity){
       ArrayAdapter<String> adapter = new ArrayAdapter<String>(activity,
               android.R.layout.simple_spinner_item, ciudades);
