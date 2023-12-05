caab128abf2a5f6a0562c5479cf30f99dc91a8a1
Javier Rivera <javier.rivera.electronics@gmail.com>
2023-12-04 20:42:23 -0500

    Se agrega permiso para el uso de la camara, y re realiza prueba de ajuste.

--- app/src/main/java/com/example/ecogardenapp2/Login.java
+++ app/src/main/java/com/example/ecogardenapp2/Login.java
@@ -10,6 +10,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
