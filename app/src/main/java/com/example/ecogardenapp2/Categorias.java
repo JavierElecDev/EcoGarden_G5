6fbb27ab2146f710f84c3d66b533174b7438436f
Javier Rivera <javier.rivera.electronics@gmail.com>
2023-12-13 01:45:45 -0500

    Se agregan acciones a los imageview de la ventanade categorias.

--- app/src/main/java/com/example/ecogardenapp2/Categorias.java
+++ app/src/main/java/com/example/ecogardenapp2/Categorias.java
@@ -7,13 +7,22 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 
 public class Categorias extends AppCompatActivity {
+
+    private ImageView imgEstadistica, imgAgua, imgAbono, imgEnergia, imgCrecimiento;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_categorias);
+
+        imgAbono = (ImageView)findViewById(R.id.cate_imgAbono);
+        imgAgua = (ImageView) findViewById(R.id.cate_imgAgua);
+        imgCrecimiento = (ImageView)findViewById(R.id.cate_imgCresimiento);
+        imgEnergia = (ImageView) findViewById(R.id.cate_imgEnergia);
+        imgEstadistica = (ImageView) findViewById(R.id.cate_imgEstadisticas);
 
         Button abono = findViewById(R.id.cateAbono);
         Button agua = findViewById(R.id.cateAgua);
@@ -22,12 +31,64 @@ public class Categorias extends AppCompatActivity {
         Button estadisticas = findViewById(R.id.cateEstadisticas);
         ImageButton back = findViewById(R.id.btn_back);
 
+
+        imgAbono.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent abonoActividad = new Intent(Categorias.this, RegistroAbono.class);
+                startActivity(abonoActividad);
+
+            }
+        });
+
+
         abono.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
                 Intent abonoActividad = new Intent(Categorias.this, RegistroAbono.class);
                 startActivity(abonoActividad);
+
+            }
+        });
+
+        imgAgua.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent aguaActividad = new Intent(Categorias.this, RegistroAgua.class);
+                startActivity(aguaActividad);
+
+            }
+        });
+
+        imgEnergia.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent energiaActividad = new Intent(Categorias.this, RegistroEnergia.class);
+                startActivity(energiaActividad);
+
+            }
+        });
+
+        imgCrecimiento.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent crecimientoActividad = new Intent(Categorias.this, RegistroCrecimiento.class);
+                startActivity(crecimientoActividad);
+
+            }
+        });
+
+        imgEstadistica.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent estadisticasActividad = new Intent(Categorias.this, EstadisticaAgua.class);
+                startActivity(estadisticasActividad);
 
             }
         });
